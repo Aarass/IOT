cfc78d48c173ff90dc6ec68e5021076ef462b1e8
Aarass <prokopovic75@gmail.com>
2025-10-20 20:36:01 +0200

    nats starting point

--- MQTTClient/index.ts
+++ MQTTClient/index.ts
@@ -1,5 +1,6 @@
-import mqtt from "mqtt";
 import "dotenv/config";
+import mqtt from "mqtt";
+import nats, { StringCodec } from "nats";
 
 const mqttClientId = crypto.randomUUID();
 
@@ -11,12 +12,31 @@ if (process.env.MQTT_PORT === undefined) {
   throw "There is no MQTT_HOST env variable";
 }
 
+if (process.env.NATS_HOST === undefined) {
+  throw "There is no NATS_HOST env variable";
+}
+
+if (process.env.NATS_PORT === undefined) {
+  throw "There is no NATS_HOST env variable";
+}
+
 let mqttClient = await mqtt.connectAsync({
   host: process.env.MQTT_HOST,
   port: parseInt(process.env.MQTT_PORT),
   clientId: mqttClientId,
   clean: true,
 });
+
+console.log("Succesfully connected to the mqqt broker");
+
+const natsClient = await nats.connect({
+  servers: `${process.env.NATS_HOST}:${process.env.NATS_PORT}`,
+});
+
+console.log("Succesfully connected to the nats broker");
+
+//
+//
 
 mqttClient.subscribeAsync("abnormal");
 
@@ -33,3 +53,20 @@ mqttClient.on("message", (topic, message) => {
   }
 });
 
+//
+//
+
+const sc = StringCodec();
+const sub = natsClient.subscribe("tmp");
+
+(async () => {
+  for await (const m of sub) {
+    console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+  }
+  console.log("subscription closed");
+})();
+
+natsClient.publish("tmp", sc.encode("world"));
+natsClient.publish("tmp", sc.encode("again"));
+
+await natsClient.drain();
